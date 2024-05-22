be4990a4d6f851fed545cf06edf5869a933f518b
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-05-21 23:12:05 -0300

    create struct in module

--- include/accounting.h
+++ include/accounting.h
@@ -2,6 +2,13 @@
 #ifndef ACCOUNTING_H
 #define ACCOUNTING_H
 
+struct accountForm {
+ 	char description[100];  
+	float value;
+};
+
+typedef struct accountForm AccountForm;
+
 int isIncoming(int opc);
 int isExpense(int opc);
 int isSeeListMovements(int opc);
